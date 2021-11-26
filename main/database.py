5b8905fa0382c6dd73b9cd563032e7e3adbf254a
Billy <72440832+billybot482@users.noreply.github.com>
2021-11-27 00:23:50 +0800

    Update database.py

--- main/database.py
+++ main/database.py
@@ -1,36 +1,42 @@
-import sqlite3
+import os
+import psycopg2
 
-def init(name):
-  return sqlite3.connect(name, check_same_thread=False)
+def init(url):
+    global conn, cur
+    conn = psycopg2.connect(url, sslmode='require')
+    cur = conn.cursor()
+    print(url)
 
-def setup(conn):
-    conn.execute("""CREATE TABLE IF NOT EXISTS User
+def setup():
+    conn.autocommit = True
+    cur.execute("SELECT VERSION();")
+    print("Connected to", cur.fetchone())
+    cur.execute("""CREATE TABLE IF NOT EXISTS Usr
             (
-                  user_id int,
-                  white int,
-                  red int,
-                  orange int,
-                  yellow int,
-                  blue int,
-                  purple int,
-                  black int,
-                  wager int ,
-                  worth int ,
-                  win int ,
-                  loss int,
-                  vip int
-
-            )
+                 user_id int not null primary key,
+                  white integer,
+                  red integer,
+                  orange integer,
+                  yellow integer,
+                  blue integer,
+                  purple integer,
+                  black integer,
+                  wager integer ,
+                  worth integer ,
+                  win integer ,
+                  loss integer,
+                  vip integer
+            );
     """)
     conn.commit()
-    conn.execute("""CREATE TABLE IF NOT EXISTS title
+    cur.execute("""CREATE TABLE IF NOT EXISTS title
                     (
-                          title TEXT
+                          title text
 
                     )
             """)
     conn.commit()
-    conn.execute("""CREATE TABLE IF NOT EXISTS Club
+    cur.execute("""CREATE TABLE IF NOT EXISTS Club
                     (
                           club1 TEXT,
                           club2 TEXT,
@@ -41,7 +47,7 @@ def setup(conn):
             """)
     conn.commit()
 
-def add_user(conn, user_id):
+def add_user(user_id):
   stmt = """INSERT INTO User (user_id, white , red , orange , yellow , blue , purple , black , wager, worth , win , loss , vip)
   VALUES (
   ?,
@@ -58,12 +64,12 @@ def add_user(conn, user_id):
   0,
   0
 )"""
-  conn.execute(stmt, (user_id,))
+  cur.execute(stmt, (user_id,))
   conn.commit()
   return conn
 
-def get_user_value(conn, user_id: int, items: str):
-    stmt = f"SELECT {items} FROM User WHERE user_id={user_id}"
+def get_user_value(user_id: int, items: str):
+    stmt = f"SELECT {items} FROM User WHERE user_id=%s;"
     try:
      r= conn.execute(stmt).fetchone()[0]
     except TypeError:
@@ -71,37 +77,37 @@ def get_user_value(conn, user_id: int, items: str):
     finally:
      return r
 
-def add_white(conn , user_id : int , white : int):
-    stmt = f"UPDATE User SET white = white + ? WHERE user_id =?"
-    conn.execute(stmt, (white,user_id))
+def add_white( user_id : int , white : int):
+    stmt = f"UPDATE User SET white = white + %s WHERE user_id =%s;"
+    cur.execute(stmt, (white,user_id))
     conn.commit()
 
-def add_red(conn , user_id : int , white : int):
-    stmt = f"UPDATE User SET red = red + ? WHERE user_id =?"
-    conn.execute(stmt, (white,user_id))
+def add_red(user_id : int , white : int):
+    stmt = f"UPDATE User SET red = red + %s WHERE user_id %s;"
+    cur.execute(stmt, (white,user_id))
     conn.commit()
 
-def add_orange(conn , user_id : int , white : int):
-    stmt = f"UPDATE User SET orange = orange + ? WHERE user_id =?"
-    conn.execute(stmt, (white,user_id))
+def add_orange(user_id : int , white : int):
+    stmt = f"UPDATE User SET orange = orange + %s WHERE user_id =%s;"
+    cur.execute(stmt, (white,user_id))
     conn.commit()
 
-def add_yellow(conn , user_id : int , white : int):
-    stmt = f"UPDATE User SET yellow = yellow + ? WHERE user_id =?"
-    conn.execute(stmt, (white,user_id))
+def add_yellow(user_id : int , white : int):
+    stmt = f"UPDATE User SET yellow = yellow + %s WHERE user_id =%s;"
+    cur.execute(stmt, (white,user_id))
     conn.commit()
 
-def add_blue(conn , user_id : int , white : int):
-    stmt = f"UPDATE User SET blue = blue + ? WHERE user_id =?"
-    conn.execute(stmt, (white,user_id))
+def add_blue( user_id : int , white : int):
+    stmt = f"UPDATE User SET blue = blue + %s WHERE user_id =%s;"
+    cur.execute(stmt, (white,user_id))
     conn.commit()
 
-def add_purple(conn , user_id : int , white : int):
-    stmt = f"UPDATE User SET purple = purple + ? WHERE user_id =?"
-    conn.execute(stmt, (white,user_id))
+def add_purple(user_id : int , white : int):
+    stmt = f"UPDATE User SET purple = purple + %s WHERE user_id =%s;"
+    cur.execute(stmt, (white,user_id))
     conn.commit()
 
-def add_black(conn , user_id : int , white : int):
-    stmt = f"UPDATE User SET black = black + ? WHERE user_id =?"
-    conn.execute(stmt, (white,user_id))
+def add_black(user_id : int , white : int):
+    stmt = f"UPDATE User SET black = black + %s WHERE user_id =%s;"
+    cur.execute(stmt, (white,user_id))
     conn.commit()
