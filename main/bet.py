5df95a7ce0ab4e9bb7c3d997f2bbd4f3a660f31c
Billy <72440832+billybot482@users.noreply.github.com>
2021-12-07 23:40:24 +0800

    Update bet.py

--- main/bet.py
+++ main/bet.py
@@ -8,6 +8,25 @@ from main import database as DB
 ONE , TWO , THREE , FOUR , FIRST , SECOND,  *_ = range(50)
 
 def bet(update, context):
+    '''Chat = update.effective_chat
+        if update.effective_chat.type != Chat.PRIVATE:
+            update.message.reply_text("play in pm")
+            return -1'''
+    cd = context.chat_data
+    query = update.callback_query
+    id = update.effective_user.id
+    name = update.effective_user.first_name
+    username = update.effective_user.name
+    VIP = DB.get_user_value(id, "vip")
+    cd["worth"] = worth = DB.get_user_value(id, "worth")
+    cd["white"] = white = DB.get_user_value(id, "white")
+    cd["red"] = red = DB.get_user_value(id, "red")
+    cd["orange"] = orange = DB.get_user_value(id, "orange")
+    cd["yellow"] = yellow = DB.get_user_value(id, "yellow")
+    cd["blue"] = blue = DB.get_user_value(id, "blue")
+    cd["purple"] = purple = DB.get_user_value(id, "purple")
+    cd["black"] = black = DB.get_user_value(id, "black")
+    amount = message.text.split
     
 
 
