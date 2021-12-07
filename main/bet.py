1bc768952d4739ae231b0045f410be94f50a5959
Billy <72440832+billybot482@users.noreply.github.com>
2021-12-07 23:38:08 +0800

    Update bet.py

--- main/bet.py
+++ main/bet.py
@@ -1,1 +1,15 @@
+import logging
+from main import dispatcher 
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import CommandHandler, InlineQueryHandler, ConversationHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ParseMode
+from telegram.ext import Updater, CallbackQueryHandler, CallbackContext , Filters
+from main import database as DB
+ONE , TWO , THREE , FOUR , FIRST , SECOND,  *_ = range(50)
 
+
+
+
+
+
+dispatcher.add_handler(BET_HANDLER)
