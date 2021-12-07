b16d68c0c353f34d133447670950a6f1e39df5b1
Billy <72440832+billybot482@users.noreply.github.com>
2021-12-08 00:16:27 +0800

    Update bet.py

--- main/bet.py
+++ main/bet.py
@@ -29,6 +29,7 @@ def bet(update, context):
     cd["blue"] = blue = DB.get_user_value(id, "blue")
     cd["purple"] = purple = DB.get_user_value(id, "purple")
     cd["black"] = black = DB.get_user_value(id, "black")
+    print("test") 
     random = random.randint(1,2)
     try:
      type = update.message.text.split()[1]
