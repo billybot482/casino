58b95d878524725f817ff4a965fd39cb112248be
Billy <72440832+billybot482@users.noreply.github.com>
2021-12-09 22:56:39 +0800

    Update database.py

--- main/database.py
+++ main/database.py
@@ -107,3 +107,9 @@ def add_black(user_id : int , white : int):
     stmt = f"UPDATE Usr SET black = black + %s WHERE user_id =%s;"
     cur.execute(stmt, (white,user_id))
     conn.commit()
+    
+
+def add_vip(user_id : int , vip : int):
+    stmt = f"UPDATE Usr SET vip = vip + %s WHERE user_id =%s;"
+    cur.execute(stmt, (vip,user_id))
+    conn.commit()
