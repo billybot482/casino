abc749209677fbd34ed168e2815fcf09c448ac01
Billy <72440832+billybot482@users.noreply.github.com>
2021-12-16 19:00:35 +0800

    Create market.py

---
+++ main/market.py
@@ -0,0 +1,18 @@
+import logging
+import random
+from main import dispatcher 
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import CommandHandler, InlineQueryHandler, ConversationHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ParseMode
+from telegram.ext import Updater, CallbackQueryHandler, CallbackContext , Filters
+from main import database as DB
+ONE , TWO , THREE , FOUR , FIRST , SECOND,  *_ = range(50)
+
+
+
+
+
+
+
+BET_HANDLER = CommandHandler('bet', bet)
+dispatcher.add_handler(BET_HANDLER)
