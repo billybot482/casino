0ded5c34897412e6550bcc5216308d47764157e1
Billy <72440832+billybot482@users.noreply.github.com>
2021-12-16 19:10:38 +0800

    Update market.py

--- main/market.py
+++ main/market.py
@@ -8,11 +8,24 @@ from telegram.ext import Updater, CallbackQueryHandler, CallbackContext , Filter
 from main import database as DB
 ONE , TWO , THREE , FOUR , FIRST , SECOND,  *_ = range(50)
 
+def market(update, context):
+    update.message.reply_text("This is market : under building") 
+
+def blackmarket(update, context):
+    id = update.effective_user.id
+    vip = DB.get_user_value(id,"vip")
+    if vip>5:
+     update.message.reply_text("This is black market") 
+    else:
+     update.message.reply_text("Kids are not allowed in black market") 
+    
 
 
 
 
 
-
-BET_HANDLER = CommandHandler('bet', bet)
+MARKET_HANDLER = CommandHandler('market', market)
 dispatcher.add_handler(BET_HANDLER)
+
+BLACK_HANDLER = CommandHandler('blackmarket', blackmarket)
+dispatcher.add_handler(BLACK_HANDLER)
