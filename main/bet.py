e27c71487fadd5d8b342facbed4f33d67158880f
Billy <72440832+billybot482@users.noreply.github.com>
2021-12-22 01:12:44 +0800

    Update bet.py

--- main/bet.py
+++ main/bet.py
@@ -10,7 +10,7 @@ ONE , TWO , THREE , FOUR , FIRST , SECOND,  *_ = range(50)
 values = {'white': 1, 'red': 5, 'orange': 25, 'yellow': 100, 'blue': 500, 'purple': 2000, 'black': 15000}
 colour = ['white', 'red', 'orange', 'yellow', 'blue', 'purple', 'black']
 rb = ['rbwhite', 'rbred', 'rborange', 'rbyellow', 'rbblue', 'rbpurple', 'rbblack']
-c = [white, red, orange, yellow, blue, purple, black]
+c = {1:white, 2:red, 3:orange, 4:yellow, 5:blue, 6:purple, 7:black}
 def bet(update, context):
     Chat = update.effective_chat
     if update.effective_chat.type != Chat.PRIVATE:
