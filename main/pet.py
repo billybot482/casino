25a69b4f747dc6e15fc706f06f49f22a21f97e6a
Billy <72440832+billybot482@users.noreply.github.com>
2022-01-05 21:25:38 +0800

    Update pet.py

--- main/pet.py
+++ main/pet.py
@@ -173,8 +173,6 @@ def check2(update ,context):
    text1 = f"\n<b>Base stats</b>\n🔆 <b>Talent :</b> <code>{talent}/{max_talent}</code>\n♨️ <b>Distract :</b> <code>{distract}/{max_distract}</code>\n❤‍🔥 <b>Confident : </b><code>{confident}/{max_confident}</code>\n\n<b>Rarity : <u>{rarity}</u></b>"
    text2 = f'<b>{type} #{str(query.data).zfill(3)}</b>\n\n<b>Growth level : {age}</b>\n🔆 <b>Talent :</b> <code>{talent}</code>\n♨️ <b>Distract :</b> <code>{distract}</code>\n❤‍🔥 <b>Confident : </b><code>{confident}</code>\n'
    
-   context.bot.delete_message(chat_id = update.effective_chat.id, message_id = msg.message_id)
-   
    context.bot.send_message(chat_id = update.effective_chat.id , photo = img, caption = text2 + text1 ,parse_mode = ParseMode.HTML, reply_markup = reply_markup)
    return FIVE
    
